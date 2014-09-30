db2e2e864991faca88c1b4b29b7d17e94158b8e8
lights li <lightsever@hotmail.com>
2014-09-30 11:08:48 +0800

    rrest

---
+++ maingame/Assets/code/logicmodel/gamemodels/BlockSceneModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+    class BlockSceneModel:IBlockSceneModel
+    {
+        IGameForModel game;
+        public BlockSceneModel(IGameForModel game)
+        {
+            this.game = game;
+        }
+        public void Update(float delta)
+        {
+            
+
+        }
+
+        public void BeginInit()
+        {
+            inited = true;
+        }
+
+        public bool inited
+        {
+            get;
+            private set;
+        }
+
+        public void BeginExit()
+        {
+            exited = true;
+        }
+
+        public bool exited
+        {
+            get;
+            private set;
+        }
+
+        public GameObject createMap(int x, int y)
+        {
+            GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            obj.transform.parent = game.rootScene.transform;
+            return obj;
+        }
+    }
+
